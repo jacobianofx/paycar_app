a867963206de6f095cd9be797c1eb562a1a2717d
Diego Alarcón Luque <diegoalarconluque@outlook.com>
2025-06-23 21:12:30 -0500

    Creaccion de Scaffolding y pantalla login

---
+++ lib/presentation/screens/login_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
+  double _scale = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 200), () {
+      setState(() {
+        _scale = 1.0; // escala completa
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedScale(
+              scale: _scale,
+              duration: const Duration(milliseconds: 800),
+              curve: Curves.easeOutBack,
+              child: Image.asset('lib/assets/logo.png', height: 80),
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Correo electrónico',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 12),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Contraseña',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Iniciar Sesión'),
+              ),
+            ),
+            /*const SizedBox(height: 12),
+            TextButton(
+              onPressed: () {},
+              child: const Text('¿Olvidaste tu contraseña?'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Crear una cuenta nueva'),
+            ),*/
+          ],
+        ),
+      ),
+    );
+  }
+}
