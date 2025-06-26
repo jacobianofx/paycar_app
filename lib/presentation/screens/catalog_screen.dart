129f3abaf5e8dc715373cb3c22d0ee402ded535e
Diego Alarcón Luque <diegoalarconluque@outlook.com>
2025-06-25 19:16:19 -0500

    Modificacion de catalogo

--- lib/presentation/screens/catalog_screen.dart
+++ lib/presentation/screens/catalog_screen.dart
@@ -1,51 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:paycar_app/presentation/screens/shopping';
 
-class CatalogScreen extends StatefulWidget {
+class CatalogScreen extends StatelessWidget {
   const CatalogScreen({super.key});
 
   @override
-<<<<<<< HEAD
   Widget build(BuildContext context) {
     final List<Map<String, String>> products = [
       {
         'title': 'Cartulina Opalina A4 180 Gr. x 25 Hojas',
         'price': 'S/9.00',
-        'image': 'lib/assets/opalina.png',
+        'image': 'assets/opalina.png',
       },
       {
         'title': 'Block cartulina de colores arcoíris A4 x 20 hojas Justus',
         'price': 'S/9.00',
-        'image': 'lib/assets/arcoiris.png',
+        'image': 'assets/arcoiris.png',
       },
     ];
-=======
-  State<CatalogScreen> createState() => _CatalogScreenState();
-}
->>>>>>> bebb7ec6fc219a090531f26a406f43811874d911
 
-class _CatalogScreenState extends State<CatalogScreen> {
-  final List<Map<String, String>> cart = [];
-  final List<Map<String, String>> products = [
-    {'title': 'Libro de Arquitectura', 'price': 'S/ 45.00'},
-    {'title': 'Arte y Diseño Moderno', 'price': 'S/ 60.00'},
-    {'title': 'Historia del Perú', 'price': 'S/ 30.00'},
-    {'title': 'Cómic Ilustrado', 'price': 'S/ 25.00'},
-  ];
-  void addtocart(Map<String, String> product) {
-    setState(() {
-      cart.add(product);
-    });
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('"${product['title']}" agregando al carrito')),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-<<<<<<< HEAD
         title: const Text(
           'Productos',
           style: TextStyle(fontWeight: FontWeight.bold),
@@ -53,7 +27,7 @@ class _CatalogScreenState extends State<CatalogScreen> {
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 16.0),
-            child: Image.asset('lib/assets/icon.png', height: 32),
+            child: Image.asset('assets/logo.png', height: 32),
           ),
         ],
       ),
@@ -139,36 +113,6 @@ class _CatalogScreenState extends State<CatalogScreen> {
                     ),
                   );
                 },
-=======
-        title: const Text('Catálogo'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => CarShop(carts: cart)),
-              );
-            },
-            icon: const Icon(Icons.shopping_cart),
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          final product = products[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: ListTile(
-              leading: const Icon(Icons.book, color: Color(0xFF215EAB)),
-              title: Text(product['title']!),
-              subtitle: Text(product['price']!),
-              trailing: IconButton(
-                icon: const Icon(Icons.add_shopping_cart),
-                onPressed: () => addtocart(product),
-
-                // Aquí podrías añadir lógica para agregar al carrito
->>>>>>> bebb7ec6fc219a090531f26a406f43811874d911
               ),
             ),
           ],
